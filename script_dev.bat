9d36846be8e7aa2ebf807de955d9c3c2cd8511fd
Usuario01 <mxarles@gmail.com>
2022-03-16 10:09:58 +0100

    script changes

--- script_dev.bat
+++ script_dev.bat
@@ -1,13 +1,25 @@
 @echo off
 
 :: last 17 positions of the path
-set end=%cd:~-17%
+set endPath=%cd:~-17%
 
-if "%end%" == "no_solo_padel_dev" (
-	:: development
-	echo Copiar pubspec.yaml a dev y prod
-	dart run utilities\development.dart
-) else (
+echo Development environment
+
+if NOT "%endPath%" == "no_solo_padel_dev" (
 	echo Wrong environment!!!
-	echo %end%
+	goto end
 )
+
+
+set /p resp="Copy pubspec to pubspec dev & prod (s/N)?: "
+if /I "%resp%"=="s" (
+   dart run utilities\development.dart
+)
+
+
+set /p resp="git push to repository (s/N)?: "
+if /I "%resp%"=="s" (
+    git push origin master
+)
+
+:end
--- script_prod.bat
+++ script_prod.bat
@@ -3,16 +3,23 @@
 :: last 13 positions of the path
 set endPath=%cd:~-13%
 
-echo Copiar pubspec_prod.yaml a pubspec.yaml
+echo Production environment
 
-if NOT "%endPath%"=="no_solo_padel" ( 
+if NOT "%endPath%"=="no_solo_padel" (
 	echo Wrong environment!!!
 	goto end
 )
 
-set /p resp="Continuar (s/N)?: "
-if /I NOT "%resp%"=="s" (goto end)
 
-dart run "utilities\production.dart"
+set /p resp="git pull from repository (s/N)?: "
+if /I "%resp%"=="s" (
+    git pull origin master
+)
+
+set /p resp="Copy pubspec_prod to pubspec (s/N)?: "
+if /I "%resp%"=="s" (
+    dart run "utilities\production.dart"
+)
 
 :end
+
