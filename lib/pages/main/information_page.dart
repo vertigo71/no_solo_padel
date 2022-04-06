0e85a6717b055f5fa1c75baf6d339505c26b4971
vertigo71 <mxarles@gmail.com>
2022-04-06 15:34:25 +0200

    "new version"

--- lib/pages/main/information_page.dart
+++ lib/pages/main/information_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:no_solo_padel_dev/utilities/misc.dart';
 import 'package:provider/provider.dart';
 
 import '../../database/firebase.dart';
@@ -8,6 +7,7 @@ import '../../interface/director.dart';
 import '../../models/debug.dart';
 import '../../models/match_model.dart';
 import '../../models/user_model.dart';
+import '../../utilities/misc.dart';
 import '../../utilities/theme.dart';
 
 final String _classString = 'InformationPage'.toUpperCase();
